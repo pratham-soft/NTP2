14766c18d4b8bcb4b6824126660e9fb340060105
Ashish Agrawal <ashishagrawal89@gmail.com>
2017-04-24 17:23:27 +0530

    Book Unit - Partial

--- app/controllers/bookUnitStep3.js
+++ app/controllers/bookUnitStep3.js
@@ -1,3 +1,6 @@
 app.controller("bookUnitStep3Ctrl", function($scope, $rootScope, $stateParams, $cookieStore, httpSvc){
 	$scope.pageTitle = "Book Unit - Payment Details";
+    $scope.paymentDetails = {
+        usruntpymtrec_pymttype: "1"
+    };
 });
