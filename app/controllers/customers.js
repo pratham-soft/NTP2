6fe1a83defbcf5a30e446b1b21eb42aff227ecd0
Amandeep Singh <ad08star@gmail.com>
2017-05-18 03:54:05 +0530

    cancel unit module partially done.

--- app/controllers/customers.js
+++ app/controllers/customers.js
@@ -56,6 +56,20 @@ app.controller("customerCtrl", function($scope, $http, $cookieStore, $state, $ui
             }
         });
     };
+    
+    $scope.unitOperations = function(selectedItem) {
+        var modalInstance = $uibModal.open({
+            templateUrl: 'customerReceivePaymentDetail.html',
+            controller: 'unitOperationCtrl',
+            size: 'lg',
+            backdrop: 'static',
+            resolve: {
+                item: function() {
+                    return selectedItem;
+                }
+            }
+        });
+    };
 });
 
 app.controller("customerDetailCtrl", function($scope, $http, $cookieStore, $state, $uibModalInstance, item,$window) {
@@ -146,3 +160,187 @@ app.controller("customerDetailCtrl", function($scope, $http, $cookieStore, $stat
         return typeName;
     }
 });
+
+
+
+app.controller("unitOperationCtrl", function($scope, $http, $cookieStore, $state, $uibModalInstance, item,$window,$uibModal,httpSvc) {
+    $scope.customer = item;
+    $scope.unitStatus = [];
+    $scope.unitStatus[2] = "Interested";
+    $scope.unitStatus[4] = "Blocked by paying advance";
+    $scope.unitStatus[5] = "Blocked by not paying advance";
+    $scope.unitStatus[6] = "Sold";
+    $scope.unitStatus[7] = "Cancelled";
+    $scope.payHistoryClick=false;
+    $scope.cancelUnitClick=false;
+   
+    if ($scope.customer.userprojlist != null) {
+        $scope.leadProjects = [];
+        for (i = 0; i < $scope.customer.userprojlist.length; i++) {
+            $scope.leadUnitObj = $scope.customer.userprojlist[i];
+            $scope.leadUnitObj.unitViewStatus = "N/A";
+            if ($scope.customer.userprojlist[i].ProjDtl_Status != 0)
+                $scope.leadUnitObj.unitViewStatus = $scope.unitStatus[$scope.customer.userprojlist[i].ProjDtl_Status];
+            $scope.leadProjects.push($scope.leadUnitObj);
+        }
+    }  
+
+    $scope.leadId = $scope.customer.user_id;
+    
+    
+    $scope.exchangeLeadUnits = function(unitObj) {
+        $window.sessionStorage.setItem('projId', unitObj.ProjId);
+        $window.sessionStorage.setItem('phaseId', unitObj.Phase_Id);
+        $window.sessionStorage.setItem('blockId', unitObj.Blocks_Id);
+        $window.sessionStorage.setItem('unitId', unitObj.UnitDtls_Id);
+        $cookieStore.put("unitObj",unitObj);
+        $uibModalInstance.close();
+        $state.go("/ExchangeUnit", {
+            "leadID": $scope.leadId
+        });
+    };
+    
+    
+     $scope.getCustPaymentHistory = function(custinfo){ 
+            $scope.payHistoryClick=true;
+            var apiPostObj ={};     
+            apiPostObj["UnitDtls_Id"] = custinfo.UnitDtls_Id;
+            apiPostObj["UnitDtls_Cust_UserId"] = $scope.customer.user_id;
+            apiPostObj["UnitDtls_comp_guid"] = $cookieStore.get('comp_guid');
+         
+			httpSvc.CustPaymentInfo(apiPostObj).then(function(response){
+				var res = response.data[0].ErrorDesc;
+                if(res=="0")
+                    {
+                        $scope.custPayinfo=response.data;
+                      
+                    }
+                 else
+                    {
+                     alert(res.toString());
+                    $scope.payHistoryClick=false;
+                    }
+			})
+        
+    }
+     
+    $scope.cancelUnit = function(unitObj) {
+        $scope.cancelUnitClick=true;
+        var apiPostObj ={};  
+        apiPostObj["UnitDtls_Id"] = unitObj.UnitDtls_Id;
+            apiPostObj["UnitDtls_Cust_UserId"] = $scope.customer.user_id;
+            apiPostObj["UnitDtls_comp_guid"] = $cookieStore.get('comp_guid');
+         
+			httpSvc.CustPaymentInfo(apiPostObj).then(function(response){
+				var res = response.data[0].ErrorDesc;
+                if(res=="0")
+                    {
+                        $scope.custPayinfo=response.data;
+                      
+                    }
+                 else
+                    {
+                     alert(res.toString());
+                    $scope.cancelUnitClick=false;    
+                    }
+			})
+        
+    };
+
+   $scope.custPayment = function(selectedItem) {
+        var modalInstance = $uibModal.open({
+            templateUrl: 'custPayment.html',
+            controller: 'custPaymentCtrl',
+            size: 'lg',
+            backdrop: 'static',
+            resolve: {
+                item: function() {
+                    return selectedItem;
+                }
+            }
+        });
+    };
+    
+   
+    $scope.receivePayment = function(unitObj) {
+//        $window.sessionStorage.setItem('projId', unitObj.ProjId);
+//        $window.sessionStorage.setItem('phaseId', unitObj.Phase_Id);
+//        $window.sessionStorage.setItem('blockId', unitObj.Blocks_Id);
+//        $window.sessionStorage.setItem('unitId', unitObj.UnitDtls_Id);
+        unitObj['CustId']= $scope.leadId
+  //      $cookieStore.put("prospectId",$scope.leadId);
+  //      $cookieStore.put("receivePaymentUnitObj",unitObj);
+        $uibModalInstance.close();
+       // $state.go("/BookUnit-Step3");
+        $scope.custPayment(unitObj);
+    };
+
+    
+    
+    
+    $scope.ok = function() {
+        $uibModalInstance.close();
+    };
+
+    $scope.getTypeNameById = function(typeId) {
+        var typeName = '';
+        switch (parseInt(typeId)) {
+            case 1:
+                typeName = 'Flat';
+                break;
+            case 2:
+                typeName = 'Sites';
+                break;
+            case 3:
+                typeName = 'Villa';
+                break;
+            case 4:
+                typeName = 'Row Houses';
+                break;
+            default:
+                console.log('eror');
+        }
+        return typeName;
+    }
+});
+
+app.controller("custPaymentCtrl", function($scope, $rootScope, $stateParams, $cookieStore, $state, httpSvc,item,$uibModalInstance){
+    var unitObj=item;
+    $scope.unitinfo=[];
+    $scope.unitinfo.push(unitObj);
+    var comp_guid = $cookieStore.get('comp_guid');
+  
+    $scope.paymentDetails = {
+        usruntpymtrec_pymttype: "1"
+    };
+   
+    $scope.receivePayment = function(formName, formObj){
+        $scope.submit = true;
+        if ($scope[formName].$valid) {            
+            formObj.usruntpymtrec_user_id = unitObj.CustId;
+            formObj.usruntpymtrec_unitdtls_id = unitObj.UnitDtls_Id;
+            formObj.usruntpymtrec_comp_guid = comp_guid;
+			formObj.usruntpymtrec_bookng = "false";
+			
+			httpSvc.updatePaymentDetails(formObj).then(function(response){
+				var res = response.data.Comm_ErrorDesc;
+				resArr = res.split('|');
+				if(resArr[0] == 0){                      
+					    alert("Payment of INR " + formObj.usruntpymtrec_amtpaid +  " Received Successfully. Now Your Balance Pending Amount is INR " + resArr[3]);
+                        $state.go('ReceivePayment');
+                        $scope.ok();
+                   
+				}
+                else{
+                   alert("Some Error Occured While Receiving Payment For This UNIT"); 
+                }
+			})
+        }
+    }
+    
+      $scope.ok = function() {
+        $uibModalInstance.close();
+    };
+});
+
+
