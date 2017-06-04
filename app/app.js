0015bbf85725fc1c1dc9a92c747817273d00d08c
Ashish Agrawal <ashishagrawal89@gmail.com>
2017-06-04 13:42:48 +0530

    Unit Listing Order --

--- app/app.js
+++ app/app.js
@@ -2,3 +2,10 @@ var app = angular.module("pratham", ['ui.router', 'ui.bootstrap', 'ngCookies','7
 app.config(function($httpProvider) {
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
 });
+
+
+app.filter('reverse', function() {
+  return function(items) {
+    return items.slice().reverse();
+  };
+});
