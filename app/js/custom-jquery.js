339efb5ced1e99b96be3a9e495b5c3c1036e81f1
Ashish Agrawal <ashishagrawal89@gmail.com>
2017-06-04 16:09:51 +0530

    Hide menu items for non-admin

--- app/js/custom-jquery.js
+++ app/js/custom-jquery.js
@@ -3,4 +3,7 @@ $(function(){
     var leftPanelHeight = pageHeight - (81);
     $(".leftPanel").css("max-height",leftPanelHeight+"px");
     $(".leftPanel").css("overflow-y","auto");
-})
+	$(".pageContent").css("max-height",leftPanelHeight+"px");
+	$(".pageContent").css("overflow-y","auto");
+	$(".pageContent").css("padding-bottom","50px");
+});
