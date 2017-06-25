dd8c454d3a44064c0ecd88ebc864f5c0ecba4732
Amandeep Singh <ad08star@gmail.com>
2017-06-25 12:01:07 +0530

    Villaments chnages

--- app/controllers/alert-rules.js
+++ app/controllers/alert-rules.js
@@ -981,7 +981,7 @@ app.controller("scheduleCtrl", function($scope, $http, $cookieStore, $state, $st
     $scope.monthDates = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
     $scope.pageTitle = "Schedule Alert";
     $scope.ruleId = $stateParams.ruleId;
-    $scope.minDate = new Date().toString();
+    $scope.minDate = new Date().toDateString();
     
     ($scope.geteditRule = function() {
         $http({
